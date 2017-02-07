17f582352f19e7dbc9ae2069da73268f38a65990
poorhack <wangyf2007@163.com>
2017-02-07 20:45:53 +0800

    creat QR with logo
    logo is at D:// C338.png
    creat at D:// test.png

---
+++ src/com/QRCode/QRCode.java
@@ -0,0 +1,174 @@
+package com.QRCode;
+
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Hashtable;
+import java.util.Map;
+
+import javax.imageio.ImageIO;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.DecodeHintType;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.LuminanceSource;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.ReaderException;
+import com.google.zxing.Result;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.common.HybridBinarizer;
+
+
+public class QRCode {
+    private static final int BLACK = 0xff000000;
+    private static final int WHITE = 0xFFFFFFFF;
+
+    /**
+     * @param args
+     */
+    
+    public static void main(String[] args) {
+    	String LogoPath="D://C338.png";
+        String Content="NAD C338 WiFi 11223344 BT 11223344 WiFi Otp 12344 Serial 1234r1324#";
+        QRCode test = new QRCode();
+        File file = new File("D://test.png");
+        /**
+         * 在com.google.zxing.MultiFormatWriter类中，定义了一些我们不知道的码,二维码只是其中的一种<br>
+         * public BitMatrix encode(String contents,
+                          BarcodeFormat format,
+                          int width, int height,
+                          Map<EncodeHintType,?> hints) throws WriterException {
+            Writer writer;
+            switch (format) {
+              case EAN_8:
+                writer = new EAN8Writer();
+                break;
+              case EAN_13:
+                writer = new EAN13Writer();
+                break;
+              case UPC_A:
+                writer = new UPCAWriter();
+                break;
+              case QR_CODE:  //这里是二维码
+                writer = new QRCodeWriter();
+                break;
+              case CODE_39:
+                writer = new Code39Writer();
+                break;
+              case CODE_128:  //这个可以生成
+                writer = new Code128Writer();
+                break;
+              case ITF:
+                writer = new ITFWriter();
+                break;
+              case PDF_417:  //这个可以生成
+                writer = new PDF417Writer();
+                break;
+              case CODABAR:
+                writer = new CodaBarWriter();
+                break;
+              default:
+                throw new IllegalArgumentException("No encoder available for format " + format);
+            }
+            return writer.encode(contents, format, width, height, hints);
+          }
+
+         */
+        test.encode(Content, file, BarcodeFormat.QR_CODE, 600, 600,LogoPath, null);
+        test.decode(file);
+    }
+
+    /**
+     * 生成QRCode二维码<br> 
+     * 在编码时需要将com.google.zxing.qrcode.encoder.Encoder.java中的<br>
+     *  static final String DEFAULT_BYTE_MODE_ENCODING = "ISO8859-1";<br>
+     *  修改为UTF-8，否则中文编译后解析不了<br>
+     */
+    public void encode(String contents, File file, BarcodeFormat format, int width, int height, String logoPath, Map<EncodeHintType,?> hints) {
+        try {
+                 //消除乱码
+        	contents = new String(contents.getBytes("UTF-8"), "ISO-8859-1"); 
+            BitMatrix bitMatrix = new MultiFormatWriter().encode(contents, format, width, height);
+            writeToFile(bitMatrix, "png", file,logoPath);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 生成二维码图片<br>
+     * 
+     * @param matrix
+     * @param format
+     *            图片格式
+     * @param file
+     *            生成二维码图片位置
+     * @throws IOException
+     */
+    public static void writeToFile(BitMatrix matrix, String format, File file,String logoPath) throws IOException {
+        BufferedImage image = toBufferedImage(matrix);
+        Graphics2D gs = image.createGraphics(); 
+        Image img = ImageIO.read(new File(logoPath));  
+        gs.drawImage(img, 250, 250, null);  
+        gs.dispose();  
+        img.flush();
+        if(!ImageIO.write(image, format, file)){  
+            throw new IOException("Could not write an image of format " + format + " to " + file);    
+        }  
+    }
+
+    /**
+     * 生成二维码内容<br>
+     * 
+     * @param matrix
+     * @return
+     */
+    public static BufferedImage toBufferedImage(BitMatrix matrix) {
+        int width = matrix.getWidth();
+        int height = matrix.getHeight();
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                image.setRGB(x, y, matrix.get(x, y) == true ? BLACK : WHITE);
+            }
+        }
+        return image;
+    }
+
+    /**
+     * 解析QRCode二维码
+     */
+    @SuppressWarnings("unchecked")
+    public void decode(File file) {
+        try {
+            BufferedImage image;
+            try {
+                image = ImageIO.read(file);
+                if (image == null) {
+                    System.out.println("Could not decode image");
+                }
+                LuminanceSource source = new BufferedImageLuminanceSource(image);
+                BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+                Result result;
+                @SuppressWarnings("rawtypes")
+                Hashtable hints = new Hashtable();
+                //解码设置编码方式为：utf-8
+                hints.put(DecodeHintType.CHARACTER_SET, "utf-8");
+                result = new MultiFormatReader().decode(bitmap, hints);
+                String resultStr = result.getText();
+                System.out.println("解析后内容：" + resultStr);
+            } catch (IOException ioe) {
+                System.out.println(ioe.toString());
+            } catch (ReaderException re) {
+                System.out.println(re.toString());
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.toString());
+        }
+    }
+}
